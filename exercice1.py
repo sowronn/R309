fc24c36eb6676ae76021151a8d6027aeb24b70bf
sowronn <101816645+sowronn@users.noreply.github.com>
2023-11-06 16:56:58 +0100

    premier push avec l'exerercice 1

--- exercice1.py
+++ exercice1.py
@@ -1,5 +1,4 @@
-x=73
-y=3
+
 def divEntier(x: int, y: int) -> int:
     if x<y:
         return 0
@@ -7,11 +6,15 @@ def divEntier(x: int, y: int) -> int:
         x=x-y
         return divEntier(x,y)+1
 
-print(divEntier(x,y))
+def main():
+    x=int(input("donné la valeur de x:"))
 
-#Ce code vise à savoir quand x est inferieur a y et le +1
-#fais office de compteur pour cette opération, il affiche
-#le nombre d’itération nécessaire pour de résolution de la soustraction
+    y=int(input("donné la valeur de x:"))
+    print(divEntier(x, y))
+
+main()
+
+#Ce code recursif vise à savoir combien de fois la valeur y est dans x.
 
 #Ce code ne fonction pas avec des valeurs negative a cause du INT, pour
 #laisser la possibilité d'utiliser les nombre negatif ou a virgule la valeur
