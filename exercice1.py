9f71be946b6bbdd83576fad70bb130759f27f56c
sowronn <101816645+sowronn@users.noreply.github.com>
2023-11-07 11:22:40 +0100

    exercice 1 terminé

--- exercice1.py
+++ exercice1.py
@@ -1,16 +1,24 @@
 
 def divEntier(x: int, y: int) -> int:
-    if x<y:
+    if x < y:
         return 0
     else:
-        x=x-y
-        return divEntier(x,y)+1
+        x = x-y
+        return divEntier(x, y)+1
 
 def main():
-    x=int(input("donné la valeur de x:"))
+    try:
+        x = int(input("donné la valeur de x:"))
+        y = int(input("donné la valeur de y:"))
+        if y == 0 or x < 0 or y < 0 or y == x:
+            print("division par 0 impossible ou par un nombre negatif")
+            return main()
 
-    y=int(input("donné la valeur de x:"))
-    print(divEntier(x, y))
+    except ValueError:
+        return print("x y doivent être des entiers"), main()
+    else:
+        print(divEntier(x, y))
+
 
 main()
 
@@ -19,4 +27,7 @@ main()
 #Ce code ne fonction pas avec des valeurs negative a cause du INT, pour
 #laisser la possibilité d'utiliser les nombre negatif ou a virgule la valeur
 #FLOAT aurais été necessaire
+
+#Le value error me permet de faire des teste a l'interieur de mon code et de specifié
+#via un code erreur la raison précise car désigné du problème
 
