4be30811077cf40769df9b900227b70e4026710b
sowronn <101816645+sowronn@users.noreply.github.com>
2023-11-10 12:06:04 +0100

    ex 2 TD OK

---
+++ TD/exercice2.py
@@ -0,0 +1,27 @@
+flag = False
+
+while flag == False:
+    arg = input("nom du fichier : ")
+    try:
+        fichier = open(arg, 'r')
+
+
+    except FileNotFoundError:
+        print("le fichier n'a pas été trouvé")
+
+    except IOError:
+        print("probleme input output")
+
+    except FileExistsError:
+        print("le fichier existe deja")
+
+    except PermissionError:
+        print("permission non suffisante")
+
+    else:
+        print(fichier.read())
+        fichier.close()
+        flag = True
+
+    finally:
+        print("fin du programme")
